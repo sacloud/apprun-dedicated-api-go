a47ec00a98f9bf4fd3725d3162f8f1ef21280a07
卜部 昌平 <s-urabe@sakura.ad.jp>
2026-07-03 11:07:14 +0900

    Library EOL
    Signed-off-by: 卜部 昌平 <s-urabe@sakura.ad.jp>

--- version.go
+++ version.go
@@ -1,6 +1,8 @@
 // Copyright 2026- The sacloud/apprun-dedicated-api-go Authors
 // SPDX-License-Identifier: Apache-2.0
 
+// Deprecated: This module has moved to github.com/sacloud/sacloud-sdk-go.
+// Please migrate to that module. This repository will be archived.
 package apprun_dedicated
 
 const Version = "0.2.1"
